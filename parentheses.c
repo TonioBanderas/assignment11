ddc0748616bec914a5858011a838c16d4c0d9d02
Ole <ole.nelson.r@gmail.com>
2022-01-11 21:30:46 +0100

    finally back

---
+++ parentheses.c
@@ -0,0 +1,24 @@
+#include "base.h"
+
+bool verify_parentheses(String text) {
+    // TODO
+    return false;
+}
+
+int main(void) {
+    report_memory_leaks(true);
+
+    test_equal_i(verify_parentheses(""), true);
+    test_equal_i(verify_parentheses("Hello World"), true);
+    test_equal_i(verify_parentheses("()"), true);
+    test_equal_i(verify_parentheses("<{[()]}>"), true);
+    test_equal_i(verify_parentheses("<{[)]}>"), false);
+    test_equal_i(verify_parentheses("( Test ) "), true);
+    test_equal_i(verify_parentheses("(1+2)*[2+3+(1 + 6/5)]"), true);
+    test_equal_i(verify_parentheses("(an unmatched left parenthesis creates an unresolved tension that will stay with you all day"), false);
+    test_equal_i(verify_parentheses("< [ > ]"), false);
+    test_equal_i(verify_parentheses("<{[()]}"), false);
+    test_equal_i(verify_parentheses("(<<({[({<{<({<([[[<[{(<{(<{{<[{<{{{<<{([<<<{{[{<<[[(([{[[[([(({()}))])]]]}]))]]>>}]}}>>>])}>>}}}>}]>}}>)}>)}]>]]])>})>}>})]})>>)"), true);
+
+    return 0;
+}
